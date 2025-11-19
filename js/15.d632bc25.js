997b38003a3a118ff40269284258551516c7ce6f
Chen-z-Sheng <Chen-z-Sheng@users.noreply.github.com>
2025-11-19 12:43:27 +0000

    deploy: 4d91aac65a085071d608cca9d3f5e0e21fa12939

---
+++ js/15.d632bc25.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkvue_admin_better=self.webpackChunkvue_admin_better||[]).push([["15"],{53698:function(e,t,u){u.r(t),u.d(t,{getList:function(){return r}});var n=u(68647);function r(){return(0,n.default)({url:"https://api.vuejs-core.cn/getAd",method:"get"})}}}]);
